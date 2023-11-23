85a3e1a5b795986f96ea44b216eb165444b843d2
mohammad6vakili <mohammad6vakili@gmail.com>
2023-11-23 11:01:36 +0330

    feat(add create form): :sparkles: add new destination form

--- src/views/rules/destinations/new/index.jsx
+++ src/views/rules/destinations/new/index.jsx
@@ -1,13 +1,77 @@
 import { Fragment } from "react";
 import Breadcrumbs from "@components/breadcrumbs";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import {
+  Card,
+  CardBody,
+  Input,
+  Row,
+  Col,
+  Form,
+  Label,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from "reactstrap";
+import CustomButton from "../../../../components/button";
 
 const DestinationsNew = () => {
+  const navigate = useNavigate();
+
   return (
     <Fragment>
       <Breadcrumbs
         title="New Destination"
         data={[{ title: "New Destination" }]}
       />
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+          navigate("/rules/destinations");
+          toast.success("Successfully Created!");
+        }}
+        className="d-flex flex-column align-items-center"
+      >
+        <Card className="w-100">
+          {/* card header */}
+          <CardHeader className="border-bottom">
+            <CardTitle>New Destination Form</CardTitle>
+          </CardHeader>
+          {/* card body */}
+          <CardBody className="pt-2">
+            {/* form fields */}
+            <Row>
+              {/* DestinationId */}
+              <Col xs="12" sm="6" md="4" className="mb-1">
+                <Label className="form-label" for="mobile">
+                  DestinationId
+                </Label>
+                <Input id="DestinationId" name="DestinationId" />
+              </Col>
+              {/* Prefixes */}
+              <Col xs="12" className="mb-1">
+                <Label className="form-label" for="mobile">
+                  Prefixes
+                </Label>
+                <Input type="textarea" id="Prefixes" name="Prefixes" />
+              </Col>
+            </Row>
+          </CardBody>
+          {/* card footer */}
+          <CardFooter className="border-top d-flex justify-content-center">
+            {/* submit button */}
+            <CustomButton
+              // loading={loadings.submit}
+              type="submit"
+              color="primary"
+              style={{ minWidth: 150 }}
+            >
+              Submit
+            </CustomButton>
+          </CardFooter>
+        </Card>
+      </Form>
     </Fragment>
   );
 };
