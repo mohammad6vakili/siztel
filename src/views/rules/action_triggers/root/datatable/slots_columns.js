98992053ae85413c970254ee38eb2640ca290fca
mohammad6vakili <mohammad6vakili@gmail.com>
2023-11-26 10:33:09 +0330

    feat(add datatable): :sparkles: add action triggers datatable

---
+++ src/views/rules/action_triggers/root/datatable/slots_columns.js
@@ -0,0 +1,85 @@
+import { Button } from "reactstrap";
+import { Link } from "react-router-dom";
+import SlotAction from "../components/slot_action";
+
+export const slots_columns = [
+  {
+    name: "BalanceId",
+    minWidth: "100px",
+    selector: (row) => row.BalanceId,
+  },
+  {
+    name: "BalanceType",
+    minWidth: "100px",
+    selector: (row) => row.BalanceType,
+  },
+  {
+    name: "BalanceRatingSubject",
+    minWidth: "180px",
+    selector: (row) => row.BalanceRatingSubject,
+  },
+  {
+    name: "BalanceCategory",
+    minWidth: "140px",
+    selector: (row) => row.BalanceCategory,
+  },
+  {
+    name: "BalanceSharedGroup",
+    minWidth: "170px",
+    selector: (row) => row.BalanceSharedGroup,
+  },
+  {
+    name: "BalanceWeight",
+    minWidth: "130px",
+    selector: (row) => row.BalanceWeight,
+  },
+  {
+    name: "DestinationId",
+    minWidth: "120px",
+    selector: (row) => row.DestinationId,
+  },
+  {
+    name: "Direction",
+    minWidth: "100px",
+    selector: (row) => row.Direction,
+  },
+  {
+    name: "MinQueuedItems",
+    minWidth: "150px",
+    selector: (row) => row.MinQueuedItems,
+  },
+  {
+    name: "MinSleep",
+    minWidth: "100px",
+    selector: (row) => row.MinSleep,
+  },
+  {
+    name: "ActionsId",
+    minWidth: "120px",
+    selector: (row) => row.ActionsId,
+  },
+  {
+    name: "ThresholdType",
+    minWidth: "130px",
+    selector: (row) => row.ThresholdType,
+  },
+  {
+    name: "ThresholdValue",
+    minWidth: "130px",
+    selector: (row) => row.ThresholdValue,
+  },
+  {
+    name: "Weight",
+    minWidth: "100px",
+    selector: (row) => row.Weight,
+  },
+  {
+    name: "Actions",
+    minWidth: "120px",
+    maxWidth: "120px",
+    selector: (row) => row.id,
+    cell: (row) => {
+      return <SlotAction id={row.id} />;
+    },
+  },
+];
--- src/views/rules/action_triggers/root/index.jsx
+++ src/views/rules/action_triggers/root/index.jsx
@@ -1,13 +1,73 @@
 import { Fragment } from "react";
 import Breadcrumbs from "@components/breadcrumbs";
+import useActionTriggers from "../../../../hooks/use_action_triggers";
+import ProgressLoading from "../../../../components/progress_loading/index";
+import ReactPaginate from "react-paginate";
+import { Col, Button } from "reactstrap";
+import ActionTriggersData from "../../../../data/action_triggers.json";
+import { useNavigate } from "react-router-dom";
+import ActionCard from "./components/action_trigger_card";
 
 const ActionTriggersRoot = () => {
+  const navigate = useNavigate();
+  const { loadings, paginates, setPaginates } = useActionTriggers();
+
+  const handlePagination = (page) => {
+    console.log(page);
+    // dispatch(setGetSshKeysCurrent(page.selected));
+    // getUserSshKeys(page.selected + 1);
+  };
+
+  const CustomPagination = () => (
+    <ReactPaginate
+      previousLabel=""
+      nextLabel=""
+      forcePage={paginates.current}
+      onPageChange={(page) => handlePagination(page)}
+      pageCount={paginates.total}
+      breakLabel="..."
+      pageRangeDisplayed={2}
+      marginPagesDisplayed={2}
+      activeClassName="active"
+      pageClassName="page-item"
+      breakClassName="page-item"
+      nextLinkClassName="page-link"
+      pageLinkClassName="page-link"
+      breakLinkClassName="page-link"
+      previousLinkClassName="page-link"
+      nextClassName="page-item next-item"
+      previousClassName="page-item prev-item"
+      containerClassName="pagination react-paginate separated-pagination pagination-sm justify-content-end pe-1 mt-1"
+    />
+  );
+
   return (
     <Fragment>
-      <Breadcrumbs
-        title="Action Triggers"
-        data={[{ title: "Action Triggers" }]}
-      />
+      <Col className="position-relative" xs={12}>
+        <Breadcrumbs
+          title="Action Triggers"
+          data={[{ title: "Action Triggers" }]}
+        />
+        <Button
+          onClick={() => navigate("/rules/action_triggers/new")}
+          color="primary"
+          style={{ position: "absolute", right: 0, top: 0 }}
+        >
+          New Action Trigger
+        </Button>
+      </Col>
+      {/* datatable */}
+      {!loadings.getActionTriggers &&
+        ActionTriggersData?.map((rate, index) => (
+          <Fragment key={index}>
+            <ActionCard rate={rate} loadings={loadings} />
+          </Fragment>
+        ))}
+      {loadings.getActionTriggers ? (
+        <div className="datatable_loading_cover">
+          <ProgressLoading />
+        </div>
+      ) : null}
     </Fragment>
   );
 };
