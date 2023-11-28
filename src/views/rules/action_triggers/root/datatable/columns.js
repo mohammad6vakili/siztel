d11f193bbce8926b25fddd09382d057281fca931
mohammad6vakili <mohammad6vakili@gmail.com>
2023-11-28 20:11:14 +0330

    feat(add datatable columns): :sparkles: add action triggers datatable columns

--- src/views/rules/action_triggers/root/datatable/columns.js
+++ src/views/rules/action_triggers/root/datatable/columns.js
@@ -1,58 +1,15 @@
+import moment from "moment/moment";
+
 export const columns = [
   {
-    name: "BalanceId",
+    name: "ID",
     minWidth: "100px",
-    selector: (row) => row.BalanceId,
+    selector: (row) => row.ID,
   },
   {
-    name: "BalanceType",
-    minWidth: "100px",
-    selector: (row) => row.BalanceType,
-  },
-  {
-    name: "BalanceRatingSubject",
-    minWidth: "180px",
-    selector: (row) => row.BalanceRatingSubject,
-  },
-  {
-    name: "BalanceCategory",
-    minWidth: "140px",
-    selector: (row) => row.BalanceCategory,
-  },
-  {
-    name: "BalanceSharedGroup",
-    minWidth: "170px",
-    selector: (row) => row.BalanceSharedGroup,
-  },
-  {
-    name: "BalanceWeight",
-    minWidth: "130px",
-    selector: (row) => row.BalanceWeight,
-  },
-  {
-    name: "DestinationId",
-    minWidth: "120px",
-    selector: (row) => row.DestinationId,
-  },
-  {
-    name: "Direction",
-    minWidth: "100px",
-    selector: (row) => row.Direction,
-  },
-  {
-    name: "MinQueuedItems",
-    minWidth: "150px",
-    selector: (row) => row.MinQueuedItems,
-  },
-  {
-    name: "MinSleep",
-    minWidth: "100px",
-    selector: (row) => row.MinSleep,
-  },
-  {
-    name: "ActionsId",
-    minWidth: "120px",
-    selector: (row) => row.ActionsId,
+    name: "UniqueID",
+    minWidth: "320px",
+    selector: (row) => row.UniqueID,
   },
   {
     name: "ThresholdType",
@@ -65,8 +22,102 @@ export const columns = [
     selector: (row) => row.ThresholdValue,
   },
   {
-    name: "Weight",
+    name: "Recurrent",
     minWidth: "100px",
+    selector: (row) => row.Recurrent,
+    cell: (row) => {
+      return JSON.stringify(row.Recurrent);
+    },
+  },
+  {
+    name: "MinSleep",
+    minWidth: "100px",
+    selector: (row) => row.MinSleep,
+  },
+  {
+    name: "ExpirationDate",
+    minWidth: "100px",
+    selector: (row) => row.ExpirationDate,
+    cell: (row) => {
+      return moment(row.ExpirationDate).format("YYYY-MM-DD");
+    },
+  },
+  {
+    name: "ActivationDate",
+    minWidth: "100px",
+    selector: (row) => row.ActivationDate,
+    cell: (row) => {
+      return moment(row.ExpirationDate).format("YYYY-MM-DD");
+    },
+  },
+  {
+    name: "BalanceId",
+    minWidth: "100px",
+    selector: (row) => row.BalanceId,
+  },
+  {
+    name: "BalanceType",
+    minWidth: "100px",
+    selector: (row) => row.BalanceType,
+  },
+  {
+    name: "BalanceDestinationIds",
+    minWidth: "180px",
+    selector: (row) => row.BalanceDestinationIds,
+  },
+  {
+    name: "BalanceWeight",
+    minWidth: "130px",
+    selector: (row) => row.BalanceWeight,
+  },
+  {
+    name: "BalanceExpirationDate",
+    minWidth: "180px",
+    selector: (row) => row.BalanceExpirationDate,
+  },
+  {
+    name: "BalanceTimingTags",
+    minWidth: "160px",
+    selector: (row) => row.BalanceTimingTags,
+  },
+  {
+    name: "BalanceRatingSubject",
+    minWidth: "180px",
+    selector: (row) => row.BalanceRatingSubject,
+  },
+  {
+    name: "BalanceCategories",
+    minWidth: "150px",
+    selector: (row) => row.BalanceCategories,
+  },
+  {
+    name: "BalanceSharedGroups",
+    minWidth: "180px",
+    selector: (row) => row.BalanceSharedGroups,
+  },
+  {
+    name: "BalanceBlocker",
+    minWidth: "130px",
+    selector: (row) => row.BalanceBlocker,
+  },
+  {
+    name: "BalanceDisabled",
+    minWidth: "140px",
+    selector: (row) => row.BalanceDisabled,
+  },
+  {
+    name: "Weight",
+    minWidth: "80px",
     selector: (row) => row.Weight,
+  },
+  {
+    name: "ActionsId",
+    minWidth: "170px",
+    selector: (row) => row.ActionsId,
+  },
+  {
+    name: "MinQueuedItems",
+    minWidth: "150px",
+    selector: (row) => row.MinQueuedItems,
   },
 ];
--- src/views/rules/action_triggers/root/datatable/slots_columns.js
+++ src/views/rules/action_triggers/root/datatable/slots_columns.js
@@ -1,62 +1,16 @@
-import { Button } from "reactstrap";
-import { Link } from "react-router-dom";
+import moment from "moment";
 import SlotAction from "../components/slot_action";
 
 export const slots_columns = [
   {
-    name: "BalanceId",
+    name: "ID",
     minWidth: "100px",
-    selector: (row) => row.BalanceId,
+    selector: (row) => row.ID,
   },
   {
-    name: "BalanceType",
-    minWidth: "100px",
-    selector: (row) => row.BalanceType,
-  },
-  {
-    name: "BalanceRatingSubject",
-    minWidth: "180px",
-    selector: (row) => row.BalanceRatingSubject,
-  },
-  {
-    name: "BalanceCategory",
-    minWidth: "140px",
-    selector: (row) => row.BalanceCategory,
-  },
-  {
-    name: "BalanceSharedGroup",
-    minWidth: "170px",
-    selector: (row) => row.BalanceSharedGroup,
-  },
-  {
-    name: "BalanceWeight",
-    minWidth: "130px",
-    selector: (row) => row.BalanceWeight,
-  },
-  {
-    name: "DestinationId",
-    minWidth: "120px",
-    selector: (row) => row.DestinationId,
-  },
-  {
-    name: "Direction",
-    minWidth: "100px",
-    selector: (row) => row.Direction,
-  },
-  {
-    name: "MinQueuedItems",
-    minWidth: "150px",
-    selector: (row) => row.MinQueuedItems,
-  },
-  {
-    name: "MinSleep",
-    minWidth: "100px",
-    selector: (row) => row.MinSleep,
-  },
-  {
-    name: "ActionsId",
-    minWidth: "120px",
-    selector: (row) => row.ActionsId,
+    name: "UniqueID",
+    minWidth: "320px",
+    selector: (row) => row.UniqueID,
   },
   {
     name: "ThresholdType",
@@ -69,9 +23,103 @@ export const slots_columns = [
     selector: (row) => row.ThresholdValue,
   },
   {
-    name: "Weight",
+    name: "Recurrent",
     minWidth: "100px",
+    selector: (row) => row.Recurrent,
+    cell: (row) => {
+      return JSON.stringify(row.Recurrent);
+    },
+  },
+  {
+    name: "MinSleep",
+    minWidth: "100px",
+    selector: (row) => row.MinSleep,
+  },
+  {
+    name: "ExpirationDate",
+    minWidth: "100px",
+    selector: (row) => row.ExpirationDate,
+    cell: (row) => {
+      return moment(row.ExpirationDate).format("YYYY-MM-DD");
+    },
+  },
+  {
+    name: "ActivationDate",
+    minWidth: "100px",
+    selector: (row) => row.ActivationDate,
+    cell: (row) => {
+      return moment(row.ExpirationDate).format("YYYY-MM-DD");
+    },
+  },
+  {
+    name: "BalanceId",
+    minWidth: "100px",
+    selector: (row) => row.BalanceId,
+  },
+  {
+    name: "BalanceType",
+    minWidth: "100px",
+    selector: (row) => row.BalanceType,
+  },
+  {
+    name: "BalanceDestinationIds",
+    minWidth: "180px",
+    selector: (row) => row.BalanceDestinationIds,
+  },
+  {
+    name: "BalanceWeight",
+    minWidth: "130px",
+    selector: (row) => row.BalanceWeight,
+  },
+  {
+    name: "BalanceExpirationDate",
+    minWidth: "180px",
+    selector: (row) => row.BalanceExpirationDate,
+  },
+  {
+    name: "BalanceTimingTags",
+    minWidth: "160px",
+    selector: (row) => row.BalanceTimingTags,
+  },
+  {
+    name: "BalanceRatingSubject",
+    minWidth: "180px",
+    selector: (row) => row.BalanceRatingSubject,
+  },
+  {
+    name: "BalanceCategories",
+    minWidth: "150px",
+    selector: (row) => row.BalanceCategories,
+  },
+  {
+    name: "BalanceSharedGroups",
+    minWidth: "180px",
+    selector: (row) => row.BalanceSharedGroups,
+  },
+  {
+    name: "BalanceBlocker",
+    minWidth: "130px",
+    selector: (row) => row.BalanceBlocker,
+  },
+  {
+    name: "BalanceDisabled",
+    minWidth: "140px",
+    selector: (row) => row.BalanceDisabled,
+  },
+  {
+    name: "Weight",
+    minWidth: "80px",
     selector: (row) => row.Weight,
+  },
+  {
+    name: "ActionsId",
+    minWidth: "170px",
+    selector: (row) => row.ActionsId,
+  },
+  {
+    name: "MinQueuedItems",
+    minWidth: "150px",
+    selector: (row) => row.MinQueuedItems,
   },
   {
     name: "Actions",
