a7ace0787a3417d9f6c992cd0a1e37a558d369ed
mohammad6vakili <mohammad6vakili@gmail.com>
2023-11-30 13:09:44 +0330

    feat(add global states): :sparkles: add actionPlans global states

--- src/redux/action_plans_slice.js
+++ src/redux/action_plans_slice.js
@@ -5,14 +5,23 @@ export const actionPlansSlice = createSlice({
   name: "actionPlans",
   initialState: {
     slots: [],
+    selectedEntity: null,
+    deleteModal: false,
   },
   reducers: {
     setSlots: (state, action) => {
       state.slots = action.payload;
     },
+    setSelectedEntity: (state, action) => {
+      state.selectedEntity = action.payload;
+    },
+    setDeleteModal: (state, action) => {
+      state.deleteModal = action.payload;
+    },
   },
 });
 
-export const { setSlots } = actionPlansSlice.actions;
+export const { setSlots, setSelectedEntity, setDeleteModal } =
+  actionPlansSlice.actions;
 
 export default actionPlansSlice.reducer;
