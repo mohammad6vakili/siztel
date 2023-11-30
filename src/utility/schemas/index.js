440f94c67a74078c84f031963557ecfaf325f770
mohammad6vakili <mohammad6vakili@gmail.com>
2023-11-30 15:01:42 +0330

    feat(add FilterIDs process): :sparkles: add FilterIDs process in create charger form

--- src/utility/schemas/index.js
+++ src/utility/schemas/index.js
@@ -7,6 +7,18 @@ export const createActionSchema = yup.object({
 
 export const createChargerSchema = yup.object({
   TPid: yup.string().required("The TPid field is required."),
+  Tenant: yup.string().required("The Tenant field is required."),
+  ID: yup.string().required("The ID field is required."),
+  ActivationTime: yup
+    .string()
+    .required("The ActivationTime field is required."),
+  ExpiryTime: yup.string().required("The ExpiryTime field is required."),
+  RunID: yup.string().required("The RunID field is required."),
+  FilterIDs: yup.array().min(1).required("The FilterIDs field is required."),
+  AttributeIDs: yup
+    .array()
+    .min(1)
+    .required("The AttributeIDs field is required."),
 });
 
 // export const registerSchema = yup.object({
--- src/views/rules/chargers/new/index.jsx
+++ src/views/rules/chargers/new/index.jsx
@@ -1,7 +1,5 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Breadcrumbs from "@components/breadcrumbs";
-import { useNavigate } from "react-router-dom";
-import toast from "react-hot-toast";
 import {
   Card,
   CardBody,
@@ -13,11 +11,22 @@ import {
   CardHeader,
   CardTitle,
   CardFooter,
+  FormFeedback,
 } from "reactstrap";
+import toast from "react-hot-toast";
+import { IoMdClose } from "react-icons/io";
 import CustomButton from "../../../../components/button";
+import useChargers from "../../../../hooks/use_chargers";
+import CustomDatePicker from "../../../../components/datepicker/index";
 
 const ChargersNew = () => {
-  const navigate = useNavigate();
+  const { createChargerController, loadings } = useChargers();
+
+  const [ActivationTime, setActivationTime] = useState(null);
+  const [ExpiryTime, setExpiryTime] = useState(null);
+
+  const [FilterIDsValue, setFilterIDsValue] = useState("");
+  const [AttributeIDsValue, setAttributeIDsValue] = useState("");
 
   return (
     <Fragment>
@@ -25,8 +34,8 @@ const ChargersNew = () => {
       <Form
         onSubmit={(e) => {
           e.preventDefault();
-          navigate("/rules/chargers");
-          toast.success("Successfully Created!");
+          window.scroll({ top: 0, behavior: "smooth" });
+          createChargerController.handleSubmit();
         }}
         className="d-flex flex-column align-items-center"
       >
@@ -41,60 +50,248 @@ const ChargersNew = () => {
             <Row>
               {/* TPid */}
               <Col xs="12" sm="6" md="4" className="mb-1">
-                <Label className="form-label" for="mobile">
+                <Label className="form-label" for="TPid">
                   TPid
                 </Label>
-                <Input id="TPid" name="TPid" />
+                <Input
+                  id="TPid"
+                  name="TPid"
+                  value={createChargerController.values.TPid}
+                  onChange={createChargerController.handleChange}
+                  invalid={
+                    createChargerController.touched.TPid &&
+                    createChargerController.errors.TPid
+                  }
+                />
+                {createChargerController.touched.TPid &&
+                createChargerController.errors.TPid ? (
+                  <FormFeedback>
+                    {createChargerController.errors.TPid}
+                  </FormFeedback>
+                ) : null}
               </Col>
               {/* Tenant */}
               <Col xs="12" sm="6" md="4" className="mb-1">
-                <Label className="form-label" for="mobile">
+                <Label className="form-label" for="Tenant">
                   Tenant
                 </Label>
-                <Input id="Tenant" name="Tenant" />
+                <Input
+                  id="Tenant"
+                  name="Tenant"
+                  value={createChargerController.values.Tenant}
+                  onChange={createChargerController.handleChange}
+                  invalid={
+                    createChargerController.touched.Tenant &&
+                    createChargerController.errors.Tenant
+                  }
+                />
+                {createChargerController.touched.Tenant &&
+                createChargerController.errors.Tenant ? (
+                  <FormFeedback>
+                    {createChargerController.errors.Tenant}
+                  </FormFeedback>
+                ) : null}
               </Col>
               {/* ID */}
               <Col xs="12" sm="6" md="4" className="mb-1">
-                <Label className="form-label" for="mobile">
+                <Label className="form-label" for="ID">
                   ID
                 </Label>
-                <Input id="ID" name="ID" />
+                <Input
+                  id="ID"
+                  name="ID"
+                  value={createChargerController.values.ID}
+                  onChange={createChargerController.handleChange}
+                  invalid={
+                    createChargerController.touched.ID &&
+                    createChargerController.errors.ID
+                  }
+                />
+                {createChargerController.touched.ID &&
+                createChargerController.errors.ID ? (
+                  <FormFeedback>
+                    {createChargerController.errors.ID}
+                  </FormFeedback>
+                ) : null}
               </Col>
               {/* ActivationTime */}
               <Col xs="12" sm="6" md="4" className="mb-1">
-                <Label className="form-label" for="mobile">
+                <Label className="form-label" for="ActivationTime">
                   ActivationTime
                 </Label>
-                <Input id="ActivationTime" name="ActivationTime" />
+                <CustomDatePicker
+                  inputPlaceholder="Click to open calendar"
+                  value={ActivationTime}
+                  onChange={(value) => {
+                    setActivationTime(value);
+                    createChargerController.setFieldValue(
+                      "ActivationTime",
+                      `${value.year}-${value.month}-${value.day}T00:00:00Z`
+                    );
+                  }}
+                  inputClassName={
+                    createChargerController.touched.ActivationTime &&
+                    createChargerController.errors.ActivationTime
+                      ? "form_error"
+                      : ""
+                  }
+                />
+                {createChargerController.touched.ActivationTime &&
+                createChargerController.errors.ActivationTime ? (
+                  <FormFeedback style={{ display: "block" }}>
+                    {createChargerController.errors.ActivationTime}
+                  </FormFeedback>
+                ) : null}
               </Col>
               {/* ExpiryTime */}
               <Col xs="12" sm="6" md="4" className="mb-1">
-                <Label className="form-label" for="mobile">
+                <Label className="form-label" for="ExpiryTime">
                   ExpiryTime
                 </Label>
-                <Input id="ExpiryTime" name="ExpiryTime" />
+                <CustomDatePicker
+                  inputPlaceholder="Click to open calendar"
+                  value={ExpiryTime}
+                  onChange={(value) => {
+                    setExpiryTime(value);
+                    createChargerController.setFieldValue(
+                      "ExpiryTime",
+                      `${value.year}-${value.month}-${value.day}T00:00:00Z`
+                    );
+                  }}
+                  inputClassName={
+                    createChargerController.touched.ExpiryTime &&
+                    createChargerController.errors.ExpiryTime
+                      ? "form_error"
+                      : ""
+                  }
+                />
+                {createChargerController.touched.ExpiryTime &&
+                createChargerController.errors.ExpiryTime ? (
+                  <FormFeedback style={{ display: "block" }}>
+                    {createChargerController.errors.ExpiryTime}
+                  </FormFeedback>
+                ) : null}
               </Col>
               {/* RunID */}
               <Col xs="12" sm="6" md="4" className="mb-1">
-                <Label className="form-label" for="mobile">
+                <Label className="form-label" for="RunID">
                   RunID
                 </Label>
-                <Input id="RunID" name="RunID" />
+                <Input
+                  id="RunID"
+                  name="RunID"
+                  value={createChargerController.values.RunID}
+                  onChange={createChargerController.handleChange}
+                  invalid={
+                    createChargerController.touched.RunID &&
+                    createChargerController.errors.RunID
+                  }
+                />
+                {createChargerController.touched.RunID &&
+                createChargerController.errors.RunID ? (
+                  <FormFeedback>
+                    {createChargerController.errors.RunID}
+                  </FormFeedback>
+                ) : null}
               </Col>
               {/* Weight */}
               <Col xs="12" sm="6" md="4" className="mb-1">
-                <Label className="form-label" for="mobile">
+                <Label className="form-label" for="Weight">
                   Weight
                 </Label>
-                <Input type="number" id="Weight" name="Weight" />
+                <Input
+                  type="number"
+                  id="Weight"
+                  name="Weight"
+                  value={createChargerController.values.Weight}
+                  onChange={createChargerController.handleChange}
+                />
               </Col>
+              {/* FilterIDs */}
+              <Col xs="12" className="mb-1">
+                <Label className="form-label" for="FilterIDs">
+                  FilterIDs
+                </Label>
+                <div className="position-relative d-flex align-items-center">
+                  <Input
+                    id="FilterIDs"
+                    name="FilterIDs"
+                    value={FilterIDsValue}
+                    onChange={(e) => setFilterIDsValue(e.target.value)}
+                    invalid={
+                      createChargerController.touched.FilterIDs &&
+                      createChargerController.errors.FilterIDs
+                    }
+                  />
+                  <button
+                    type="button"
+                    style={{
+                      right: 35,
+                      borderRadius: "50%",
+                      border: "none",
+                      background: "transparent",
+                      width: 30,
+                      height: 30,
+                      border: "1px solid rgba(200,200,200,.5)",
+                      color: "white",
+                      fontSize: 18,
+                      cursor: "pointer",
+                    }}
+                    className="position-absolute"
+                    onClick={() => {
+                      if (FilterIDsValue.length === 0) {
+                        toast.error("Enter Filter ID value.");
+                      } else {
+                        let array = [
+                          ...createChargerController.values.FilterIDs,
+                        ];
+                        array.push(FilterIDsValue);
+                        createChargerController.setFieldValue(
+                          "FilterIDs",
+                          array
+                        );
+                        setFilterIDsValue("");
+                      }
+                    }}
+                  >
+                    +
+                  </button>
+                </div>
+                {createChargerController.touched.FilterIDs &&
+                createChargerController.errors.FilterIDs ? (
+                  <FormFeedback style={{ display: "block" }}>
+                    {createChargerController.errors.FilterIDs}
+                  </FormFeedback>
+                ) : null}
+              </Col>
+              {createChargerController.values.FilterIDs.map((item, index) => (
+                <Col xs="12" className="mb-1 d-flex align-items-center">
+                  <span>{index + 1} -</span>
+                  <span>{item}</span>
+                  <button
+                    type="button"
+                    style={{
+                      background: "transparent",
+                      border: "none",
+                      marginLeft: "auto",
+                    }}
+                    onClick={() => {
+                      let array = [...createChargerController.values.FilterIDs];
+                      array = array.filter((intem, i) => i !== index);
+                      createChargerController.setFieldValue("FilterIDs", array);
+                    }}
+                  >
+                    <IoMdClose style={{ fontSize: 14, color: "red" }} />
+                  </button>
+                </Col>
+              ))}
             </Row>
           </CardBody>
           {/* card footer */}
           <CardFooter className="border-top d-flex justify-content-center">
             {/* submit button */}
             <CustomButton
-              // loading={loadings.submit}
+              loading={loadings.createCharger}
               type="submit"
               color="primary"
               style={{ minWidth: 150 }}
