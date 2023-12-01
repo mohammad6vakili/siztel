f556f05dc80817d162d5515663a76b60b978e8dc
mohammad6vakili <mohammad6vakili@gmail.com>
2023-12-01 11:30:34 +0330

    feat(add global states): :sparkles: add rates global states

--- src/redux/rates_slice.js
+++ src/redux/rates_slice.js
@@ -5,14 +5,23 @@ export const ratesSlice = createSlice({
   name: "rates",
   initialState: {
     slots: [],
+    selectedEntity: null,
+    deleteModal: false,
   },
   reducers: {
     setSlots: (state, action) => {
       state.slots = action.payload;
     },
+    setSelectedEntity: (state, action) => {
+      state.selectedEntity = action.payload;
+    },
+    setDeleteModal: (state, action) => {
+      state.deleteModal = action.payload;
+    },
   },
 });
 
-export const { setSlots } = ratesSlice.actions;
+export const { setSlots, setSelectedEntity, setDeleteModal } =
+  ratesSlice.actions;
 
 export default ratesSlice.reducer;
