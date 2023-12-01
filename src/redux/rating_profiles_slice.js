75afb66c29e92409dbdc375f342f8c61699b7872
mohammad6vakili <mohammad6vakili@gmail.com>
2023-12-01 13:25:50 +0330

    feat(add global states): :sparkles: add rating profiles global state

--- src/redux/rating_profiles_slice.js
+++ src/redux/rating_profiles_slice.js
@@ -5,14 +5,23 @@ export const ratingProfiles = createSlice({
   name: "ratingProfiles",
   initialState: {
     slots: [],
+    selectedEntity: null,
+    deleteModal: false,
   },
   reducers: {
     setSlots: (state, action) => {
       state.slots = action.payload;
     },
+    setSelectedEntity: (state, action) => {
+      state.selectedEntity = action.payload;
+    },
+    setDeleteModal: (state, action) => {
+      state.deleteModal = action.payload;
+    },
   },
 });
 
-export const { setSlots } = ratingProfiles.actions;
+export const { setSlots, setSelectedEntity, setDeleteModal } =
+  ratingProfiles.actions;
 
 export default ratingProfiles.reducer;
