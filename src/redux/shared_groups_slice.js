dca81a9b416b8925ab6fee753223cc3261de6e8c
mohammad6vakili <mohammad6vakili@gmail.com>
2023-12-01 14:16:21 +0330

    feat(add global states): :sparkles: add sharedGroups global states

--- src/redux/shared_groups_slice.js
+++ src/redux/shared_groups_slice.js
@@ -5,14 +5,23 @@ export const sharedGroupsSlice = createSlice({
   name: "sharedGroups",
   initialState: {
     slots: [],
+    selectedEntity: null,
+    deleteModal: false,
   },
   reducers: {
     setSlots: (state, action) => {
       state.slots = action.payload;
     },
+    setSelectedEntity: (state, action) => {
+      state.selectedEntity = action.payload;
+    },
+    setDeleteModal: (state, action) => {
+      state.deleteModal = action.payload;
+    },
   },
 });
 
-export const { setSlots } = sharedGroupsSlice.actions;
+export const { setSlots, setSelectedEntity, setDeleteModal } =
+  sharedGroupsSlice.actions;
 
 export default sharedGroupsSlice.reducer;
