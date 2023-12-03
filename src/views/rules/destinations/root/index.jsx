560a86c024645e0c142b6af52415d5b630be81f0
mohammad6vakili <mohammad6vakili@gmail.com>
2023-12-03 21:22:32 +0330

    feat(add datatable all process): :sparkles: add destinations datatable all process

--- src/views/rules/destinations/root/index.jsx
+++ src/views/rules/destinations/root/index.jsx
@@ -1,14 +1,11 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import Breadcrumbs from "@components/breadcrumbs";
 import useDestinations from "../../../../hooks/use_destionations";
 import { useSkin } from "@hooks/useSkin";
 import ProgressLoading from "../../../../components/progress_loading/index";
 import DataTable from "react-data-table-component";
-import ReactPaginate from "react-paginate";
-import { ChevronDown } from "react-feather";
 import { columns } from "./datatable/columns";
 import { Col, Button } from "reactstrap";
-import DestinationsData from "../../../../data/destinations.json";
 import { useNavigate } from "react-router-dom";
 import Confirm from "../../../../components/confirm";
 import { useDispatch, useSelector } from "react-redux";
@@ -18,41 +15,23 @@ const DestinationsRoot = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { skin } = useSkin();
-  const { loadings, paginates, setPaginates } = useDestinations();
+  const { getDestinations, deleteEntity, listData, loadings } =
+    useDestinations();
 
   const selectedEntity = useSelector(
     (state) => state.destinations.selectedEntity
   );
   const deleteModal = useSelector((state) => state.destinations.deleteModal);
-
-  const handlePagination = (page) => {
-    console.log(page);
-    // dispatch(setGetSshKeysCurrent(page.selected));
-    // getUserSshKeys(page.selected + 1);
-  };
-
-  const CustomPagination = () => (
-    <ReactPaginate
-      previousLabel=""
-      nextLabel=""
-      forcePage={paginates.current}
-      onPageChange={(page) => handlePagination(page)}
-      pageCount={paginates.total}
-      breakLabel="..."
-      pageRangeDisplayed={2}
-      marginPagesDisplayed={2}
-      activeClassName="active"
-      pageClassName="page-item"
-      breakClassName="page-item"
-      nextLinkClassName="page-link"
-      pageLinkClassName="page-link"
-      breakLinkClassName="page-link"
-      previousLinkClassName="page-link"
-      nextClassName="page-item next-item"
-      previousClassName="page-item prev-item"
-      containerClassName="pagination react-paginate separated-pagination pagination-sm justify-content-end pe-1 mt-1"
-    />
+  const getAllTpIdsLoading = useSelector(
+    (state) => state.app.getAllTpIdsLoading
   );
+  const selectedTpId = useSelector((state) => state.app.selectedTpId);
+
+  useEffect(() => {
+    if (selectedTpId) {
+      getDestinations();
+    }
+  }, [selectedTpId]);
 
   return (
     <Fragment>
@@ -66,9 +45,24 @@ const DestinationsRoot = () => {
           New Destination
         </Button>
       </Col>
-      {/* datatable */}
       <div className="react-dataTable mv_datatable_container">
-        {!loadings.getDestinations ? (
+        {/* tpids loading */}
+        {getAllTpIdsLoading ? (
+          <div
+            style={{ width: "100%", height: "50vh" }}
+            className="d-flex justify-content-center align-items-center"
+          >
+            Loading TPIds...
+          </div>
+        ) : null}
+        {/* table loading */}
+        {!getAllTpIdsLoading && loadings.getDestinations ? (
+          <div className="datatable_loading_cover">
+            <ProgressLoading />
+          </div>
+        ) : null}
+        {/* datatable */}
+        {!getAllTpIdsLoading && !loadings.getDestinations ? (
           <Fragment>
             <DataTable
               noDataComponent={
@@ -81,22 +75,13 @@ const DestinationsRoot = () => {
                 )
               }
               noHeader
-              pagination
               columns={columns}
-              paginationPerPage={10}
               className="react-dataTable"
               style={{ background: "red" }}
-              sortIcon={<ChevronDown size={10} />}
-              paginationComponent={CustomPagination}
-              data={DestinationsData}
+              data={listData}
               theme={skin === "dark" ? "darkTheme" : ""}
             />
           </Fragment>
-        ) : null}
-        {loadings.getDestinations ? (
-          <div className="datatable_loading_cover">
-            <ProgressLoading />
-          </div>
         ) : null}
       </div>
       {/* delete modal */}
@@ -107,8 +92,8 @@ const DestinationsRoot = () => {
         noAction={() => dispatch(setDeleteModal(false))}
         noColor={"secondary"}
         noTitle={"Cancel"}
-        yesLoading={loadings.deleteDestination}
-        yesAction={() => alert(selectedEntity?.TPid)}
+        yesLoading={loadings.deleteEntity}
+        yesAction={() => deleteEntity(selectedEntity)}
         yesColor={"danger"}
         yesTitle={"Delete"}
         type={"global"}
