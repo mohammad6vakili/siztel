9f26bc9565be2599c8c1103275b96513add1f7a6
mohammad6vakili <mohammad6vakili@gmail.com>
2023-12-13 20:26:31 +0330

    feat(add action buttons): :sparkles: add accounts table action butttons

--- src/views/admin/accounts/root/components/action_button.jsx
+++ src/views/admin/accounts/root/components/action_button.jsx
@@ -1,13 +1,37 @@
 import { Fragment } from "react";
 import { Button } from "reactstrap";
+import { useDispatch } from "react-redux";
+import {
+  setDeleteModal,
+  setSelectedEntity,
+} from "../../../../../redux/accounts_slice";
+import { useNavigate } from "react-router-dom";
 
 const ActionButton = ({ row }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   return (
     <Fragment>
-      <Button style={{ padding: 6, marginRight: 8 }} size="sm" color="primary">
+      <Button
+        style={{ padding: 6, marginRight: 8 }}
+        size="sm"
+        color="primary"
+        onClick={() => {
+          navigate(`/admin/accounts/update?entity_id=${row.ID}`);
+        }}
+      >
         Update
       </Button>
-      <Button style={{ padding: 6 }} size="sm" color="danger">
+      <Button
+        style={{ padding: 6 }}
+        size="sm"
+        color="danger"
+        onClick={() => {
+          dispatch(setSelectedEntity(row.ID));
+          dispatch(setDeleteModal(1));
+        }}
+      >
         Delete
       </Button>
     </Fragment>
